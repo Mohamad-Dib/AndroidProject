004ee7974da22a11f63f1e06829f001b54c687d7
mohamadmokbel01 <mohamadmokbel01@gmail.com>
2025-01-03 13:29:21 +0200

    JobPosting

---
+++ app/src/main/java/com/example/jobportal/JobPosting.kt
@@ -0,0 +1,18 @@
+package com.example.jobportal
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+data class JobPosting(
+    val jobTitle: String = "",
+    val companyName: String = "",
+    val location: String = "",
+    val jobDescription: String = "",
+    val requiredSkills: String = "",
+    val salary: String? = null,
+    val jobType: String = "",
+    val postedBy: String = "",
+    val postedDate: Long = 0L
+)
+
